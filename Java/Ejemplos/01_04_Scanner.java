3466e279291069d215ff3386e01badab4b5d2dfe
Camilo Ernesto Rodriguez <31961588+crodrigr@users.noreply.github.com>
2022-04-01 11:40:35 -0500

    Update 01_04_Scanner.java

--- Java/Ejemplos/01_04_Scanner.java
+++ Java/Ejemplos/01_04_Scanner.java
@@ -8,10 +8,10 @@ public class HolaMundo {
     public static void main(String args[]) {
         System.out.println("Escribe tu nombre:");
         Scanner consola = new Scanner(System.in); 
-        var usuario = consola.nextLine();
+        String usuario = consola.nextLine();
         System.out.println("usuario = " + usuario);
         System.out.println("Escribe el titulo:");
-        var titulo = consola.nextLine();
+        String titulo = consola.nextLine();
         System.out.println("Resultado: " + titulo + " " + usuario);
     }
 }
