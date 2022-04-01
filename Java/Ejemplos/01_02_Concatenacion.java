37655ffa31be908be84bd333dd081aca3c12147c
Camilo Ernesto Rodriguez <31961588+crodrigr@users.noreply.github.com>
2022-04-01 11:39:15 -0500

    Update 01_02_Concatenacion.java

--- Java/Ejemplos/01_02_Concatenacion.java
+++ Java/Ejemplos/01_02_Concatenacion.java
@@ -3,13 +3,13 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var usuario = "Juan";
-        var titulo = "Ingeniero";
-        var union = titulo + " " + usuario;
+        String usuario = "Juan";
+        String titulo = "Ingeniero";
+        String union = titulo + " " + usuario;
         System.out.println("union = " + union);
         
-        var i = 3;
-        var j = 4;
+        int i = 3;
+        int j = 4;
         
         System.out.println(i + j);//se realiza la suma de numeros
         System.out.println(i + j + usuario); //Evaluación de izq a der, realiza suma
