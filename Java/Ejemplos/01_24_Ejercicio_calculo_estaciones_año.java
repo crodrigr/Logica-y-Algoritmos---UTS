48f145cfed347f16995ee9db7a828fb98832aafe
Camilo Ernesto Rodriguez <31961588+crodrigr@users.noreply.github.com>
2022-04-01 11:52:05 -0500

    Update 01_24_Ejercicio_calculo_estaciones_año.java

--- Java/Ejemplos/01_24_Ejercicio_calculo_estaciones_año.java
+++ Java/Ejemplos/01_24_Ejercicio_calculo_estaciones_año.java
@@ -3,7 +3,7 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var mes = 4;
+        int mes = 4;
         var estacion = "Estacion desconocida";
         
         if(mes == 1 || mes == 2 || mes == 12){
