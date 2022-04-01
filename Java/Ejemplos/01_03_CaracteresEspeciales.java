2765e208071306bde2d9cde0cbc420e2be6cffb5
Camilo Ernesto Rodriguez <31961588+crodrigr@users.noreply.github.com>
2022-04-01 11:39:56 -0500

    Update 01_03_CaracteresEspeciales.java

--- Java/Ejemplos/01_03_CaracteresEspeciales.java
+++ Java/Ejemplos/01_03_CaracteresEspeciales.java
@@ -3,7 +3,7 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var nombre = "Karla";
+        String nombre = "Karla";
 
         System.out.println("Nueva linea: \n" + nombre);
         System.out.println("Tabulador: \t" + nombre);
