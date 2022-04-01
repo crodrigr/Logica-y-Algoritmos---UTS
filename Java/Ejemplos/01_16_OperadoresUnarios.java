cd24a31277f4ac933fea24ea07e311a25ef9a840
Camilo Ernesto Rodriguez <31961588+crodrigr@users.noreply.github.com>
2022-04-01 11:46:39 -0500

    Update 01_16_OperadoresUnarios.java

--- Java/Ejemplos/01_16_OperadoresUnarios.java
+++ Java/Ejemplos/01_16_OperadoresUnarios.java
@@ -3,38 +3,38 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var a = 3;
-        var b = -a;
+        int a = 3;
+        int b = -a;
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         
-        var c = true;
-        var d = !c;
+        boolean c = true;
+        boolean d = !c;
         System.out.println("c = " + c);
         System.out.println("d = " + d);
         
         //incremento
         //1.preincremento (simbolo antes de la variable)
-        var e = 3;
-        var f = ++e;//primero se incrementa la variable y despues se usa su valor
+        int e = 3;
+        int f = ++e;//primero se incrementa la variable y despues se usa su valor
         System.out.println("e = " + e);
         System.out.println("f = " + f);
         //2.postincremento (simbolo despues de la variable)
-        var g = 5;
-        var h = g++;//primero se utiliza el valor y despues se incrementa
+        int g = 5;
+        int h = g++;//primero se utiliza el valor y despues se incrementa
         System.out.println("g = " + g);//teniamos pendiente un incremento
         System.out.println("h = " + h);
         
         //decremento
         //1.predecremento
-        var i = 2;
-        var j = --i;
+        int i = 2;
+        int j = --i;
         System.out.println("i = " + i);//ya esta drecrementada
         System.out.println("j = " + j);
         
         //2.postdecremento
-        var k = 4;
-        var l = k--;//primero se usa el valor de la variable y queda pendiente decremento
+        int k = 4;
+        int l = k--;//primero se usa el valor de la variable y queda pendiente decremento
         System.out.println("k = " + k);//tenia pendiente un drecremento
         System.out.println("l = " + l);
     }
