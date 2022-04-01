d8c830df8e884483ea7fa6e845d0b7052ecde095
Camilo Ernesto Rodriguez <31961588+crodrigr@users.noreply.github.com>
2022-04-01 11:50:09 -0500

    Update 01_20_PrecedenciaOperadores.java

--- Java/Ejemplos/01_20_PrecedenciaOperadores.java
+++ Java/Ejemplos/01_20_PrecedenciaOperadores.java
@@ -3,17 +3,17 @@
 public class HolaMundo {
     
     public static void main(String args[]) {
-        var x = 5;
-        var y = 10;
-        var z = ++x + y--;
+        int x = 5;
+        int y = 10;
+        int z = ++x + y--;
         System.out.println("x = " + x);
         System.out.println("y = " + y);
         System.out.println("z = " + z);
         
-        var resultado = 4 + 5 * 6 / 3;//4 + ((5*6)/3)
+        int resultado = 4 + 5 * 6 / 3;//4 + ((5*6)/3)
         System.out.println("resultado = " + resultado);//14
         
-        resultado = (4 + 5) * 6 / 3;
+        int = (4 + 5) * 6 / 3;
         System.out.println("resultado = " + resultado);
     }
 }
